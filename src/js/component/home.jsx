6bdb6a00da886232b7bfae93c3fa3bafc0e54079
Ramiro Scarinci <151871077+RamiroSca@users.noreply.github.com>
2024-02-09 10:24:01 +0000

    Proyecto terminado con el intento del Bonus

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,20 +2,73 @@ import React, { useState } from "react";
 
 
 const Home = () => {
-	const [ligth, setLigth] = useState(" ")
+	const [ligthRed, setLigthRed] = useState(" ")
+	const [ligthYellow, setLigthYellow] = useState(" ")
+	const [ligthGreen, setLigthGreen] = useState(" ")
+	const [Violeta, setVioleta] = useState(" ")
 
-	function select() {
-		setLigth(" ")
+	function selectRed() {
+		if (ligthRed == "ligthRed") {
+			setLigthRed(" ");
+		} else {
+			setLigthRed("ligthRed")
+			setLigthYellow(" ")
+			setLigthGreen(" ")
+		}
 	}
+	function selectYellow() {
+		if (ligthYellow == "ligthYellow") {
+			setLigthYellow(" ")
+		} else {
+			setLigthYellow("ligthYellow")
+			setLigthRed(" ");
+			setLigthGreen(" ")
+		}
+
+	}
+	function selectGreen() {
+		if (ligthGreen == "ligthGreen") {
+			setLigthGreen(" ")
+		} else {
+			setLigthGreen("ligthGreen")
+			setLigthRed(" ");
+			setLigthYellow(" ")
+		}
+	}
+	function todoVioleta() {
+		if (Violeta == "semaforosVioleta") {
+			setVioleta(" ")
+		} else {
+			setVioleta("semaforosVioleta")
+			setLigthGreen(" ")
+			setLigthRed(" ");
+			setLigthYellow(" ")
+		}
+	}
+
+	function automatico () {
+		let numeroAzar = Math.floor(Math.random()*3+1)
+		if (numeroAzar==1) {
+			selectRed()
+		} else if (numeroAzar==2){
+			selectYellow()
+		} else {
+			selectGreen()
+		}
+		console.log(numeroAzar)
+	}
+
 	return (
-		<div className="container">
-			<div className="bg-black text-center col-1">
+		<div className="container flex justify-content-center">
+			<div className="bg-black text-center col-1 poste mx-auto ">
 			</div>
-			<div className="bg-black col-3 p-1">
-				<div className={"rounded-circle bg-danger p-2 m-2 " + ligth} onClick={select} ></div>
-				<div className={"rounded-circle bg-warning p-2 m-2 " + ligth} onClick={select}></div>
-				<div className={"rounded-circle bg-success p-2 m-2 " + ligth} onClick={select}></div>
+			<div className="bg-black col-3 p-1 rounded semaforo mx-auto ">
+				<div className={"rounded-circle bg-danger m-1 p-2 largo " + ligthRed + Violeta} onClick={selectRed} ></div>
+				<div className={"rounded-circle bg-warning m-1 p-2 largo " + ligthYellow + Violeta } onClick={selectYellow}></div>
+				<div className={"rounded-circle bg-success m-1 p-2 largo " + ligthGreen + Violeta} onClick={selectGreen}></div>
 			</div>
+			<button className="btn botonAutomatico" onClick={automatico}> Automatico </button>
+			<button className="btn botonVioleta " onClick={todoVioleta}> Violeta </button>
 		</div>
 	);
 };
